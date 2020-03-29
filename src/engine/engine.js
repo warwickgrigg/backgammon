4b7aaf32912578c5b0aee981aabe95ce7d9cd127
Warwick Grigg <warwick.grigg@gmail.com>
2020-03-29 11:23:12 +0100

    refactor

--- src/engine/engine.js
+++ src/engine/engine.js
@@ -210,54 +210,37 @@ const cartesian = (fn, arrays = []) =>
 //cartesian(jlog, [["A", 1], [0, 1]]);
 //cartesian(jlog, [["A", "B", "C"], [0, 1], ["x", "y", "z"]]);
 
-const permx = (fn, ordered = false, n, counts) => {
-  const a = counts.slice();
-  const m = counts.length;
+const perm = (fn, ordered = false, n, count) => {
+  const a = Array.isArray(count) ? count.slice() : Array(count).fill(1);
+  const m = a.length;
   const result = Array(n);
-  const recurse = (ri, start) => {
-    for (let i = ordered ? start : 0; i < m; i++) {
+  const recurse = (r, i) => {
+    while (i < m) {
       if (a[i]) {
-        result[ri] = i;
-        if (ri === n - 1) {
+        result[r] = i;
+        if (r === n - 1) {
           fn(result.slice());
         } else {
-          a[i]--; // one less available to next level
-          recurse(ri + 1, a[i] ? i : i + 1);
+          a[i]--; // one less available
+          recurse(r + 1, ordered ? i : 0);
           a[i]++; // make available again
         }
       }
-    }
-  };
-  recurse(0, 0);
-};
-permx(jlog, true, 3, [1, 2, 1, 1]);
-const perm = (fn, ordered = false, n, m) => {
-  const result = Array(n);
-  const a = Array(m).fill(1);
-  const recurse = (ri, start) => {
-    for (let i = ordered ? start : 0; i < m; i++) {
-      if (a[i]) {
-        result[ri] = i;
-        if (ri === n - 1) {
-          fn(result.slice());
-        } else {
-          a[i]--; // not available to next level
-          recurse(ri + 1, i + 1);
-          a[i]++; // make available again
-        }
-      }
+      i++;
     }
   };
   recurse(0, 0);
 };
 
-const permArray = (fn, ordered, n, array) =>
+const permItems = (fn, ordered, n, array) =>
   perm(p => fn(p.map(e => array[e])), ordered, n, array.length);
 
+perm(jlog, true, 3, [1, 2, 1, 1]);
+perm(jlog, false, 2, 3);
 const r = [];
 perm(v => r.push(v), true, 2, 3);
 jlog({ l: r.length, r });
-permArray(jlog, true, 2, ["00", "01", "2", "3", "4"]);
+permItems(jlog, true, 2, ["A", "B", "C", "D", "E"]);
 
 /*
 const occPoints = (me, [min = 1, max = 15]) => {

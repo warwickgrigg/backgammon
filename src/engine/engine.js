85df99c70e580fc0357d188a8f118e4849919c1b
Warwick Grigg <warwick.grigg@gmail.com>
2020-03-31 19:25:31 +0100

    neat valid move generator

--- src/engine/engine.js
+++ src/engine/engine.js
@@ -7,183 +7,6 @@ const myPoints = ({ isWhite, points }) =>
     ? [points[0].slice(), points[1].slice().reverse()]
     : [points[1].slice().reverse(), points[0].slice()];
 
-/*
-const validMoves1 = ({ dice, isWhite, points }) => {
-  const moves = (pos, dice, me, opp) => {
-    const simpleMoves = (pos, dice, me, opp) => {
-      for (var i = 0, p = pos, taken = [], result = []; i < dice.length; i++) {
-        p += dice[i];
-        if (p > 24 || opp[p] > 1) return result;
-        if (opp[p] === 1) taken.push(p);
-        result.push({
-          from: pos,
-          to: p,
-          taken: taken.slice(),
-          dice: dice.slice(0, i + 1)
-        });
-        if (me[p] && me[p] + 2 + i >= dice.length) return result;
-      }
-      return result;
-    };
-    const r1 = simpleMoves(pos, dice, me, opp);
-    if (dice.length === 4) {
-      return r1;
-    } else {
-      const r2 = simpleMoves(pos, [dice[1], dice[0]], me, opp);
-      if (
-        r1.length === 2 &&
-        r2.length === 2 &&
-        !opp[r1[0].to] &&
-        !opp[r2[0].to]
-      ) {
-        return [r1[0], r2[0], r1[1]]; // eg. if both 3,4 and 4,3 equivalent
-      }
-      return r1.concat(r2); // no equivalences to eliminate
-    }
-  };
-
-  const xDice = ([d1, d2]) => (d1 === d2 ? [d1, d1, d1, d1] : [d1, d2]);
-
-  const myPoints = ({ isWhite, points }) =>
-    isWhite
-      ? [[...points[0]], [...points[1]].reverse()]
-      : [[...points[1]].reverse(), [...points[0]]];
-
-  const [me, opp] = myPoints({ isWhite, points });
-
-  for (var i = 0, result = [[], [], [], [], []]; i < me.length; i++) {
-    if (me[i]) {
-      const m = moves(i, xDice(dice), me, opp);
-      //result = result.concat(m);
-      m.forEach(m1 => result[m1.dice.length].push(m1));
-    }
-  }
-  //[[[2], [1,1], [[1,1,1,1], [1,1,2], [2,2], [4]]]
-  return result;
-};
-
-const vMovesx = ({ points, isWhite = true, dice }) => {
-  let result = [];
-  let diceCombo = d1 === d2 ? [[d1, d1, d1, d1]] : [[d1, d2], [d2, d1]];
-  let [comboNo, diceNo] = [0, 0];
-  let [me, opp] = myPoints({ isWhite, points });
-  let from = 0; // next position to consider
-  let branchMoves = []; // moves applied for this branch
-  const pushMove = (from, to) => branchMoves.push({ from, to, taken: opp[to] });
-  const popMove = () => branchMoves.pop();
-  const applyMove = (from, to) => {
-    pushMove(from, to);
-    me[from]--;
-    me[to]++;
-    opp[to] = 0;
-  };
-
-  const undoMove = () => {
-    var { from, to, taken } = popMove();
-    me[from]++;
-    me[to]--;
-    opp[to] = taken;
-    return from;
-  };
-
-  const nextPoint = () => {
-    while (from < 24 && !me[from]) from++;
-  };
-
-  nextPoint();
-
-  while (from < 24 && me[from]) {
-    [comboNo, diceNo] = [0, 0];
-    while (from < 24 && me[from]) {
-      while (from < 24 && comboNo < diceCombo.length) {
-        while (from < 24)
-          while (diceNo < diceCombo[comboNo].length) {
-            const to = from + diceCombo[comboNo][diceNo];
-            if (to < 24 && opp[to] <= 1) {
-              applyMove(from, to);
-            }
-          }
-      }
-      if (diceNo >= diceCombo[comboNo].length) {
-        result.push(branchMoves);
-      }
-      if (to <= 24) {
-        from++;
-      }
-      diceNo = 0;
-      comboNo++;
-    }
-    from++;
-  }
-
-  //diceComboIndex = [0, 0];
-  //return false; // no more moves for this puck
-  return result;
-};
-
-const vMovesy = ({ points, isWhite = true, dice }) => {
-  let result = [];
-  let diceCombo = d1 === d2 ? [[d1, d1, d1, d1]] : [[d1, d2], [d2, d1]];
-  let [comboNo, diceNo] = [0, 0];
-  let [me, opp] = myPoints({ isWhite, points });
-  let p = positions[p];
-  let from = 0; // next position to consider
-  let branchMoves = []; // moves applied for this branch
-  const pushMove = (from, to) => branchMoves.push({ from, to, taken: opp[to] });
-  const popMove = () => branchMoves.pop();
-  const applyMove = (from, to) => {
-    pushMove(from, to);
-    me[from]--;
-    me[to]++;
-    opp[to] = 0;
-  };
-
-  while (from < 24 && !me[from]) {
-    from++;
-  }
-  const undoMove = () => {
-    var { from, to, taken } = popMove();
-    me[from]++;
-    me[to]--;
-    opp[to] = taken;
-    return from;
-  };
-
-  const nextPoint = () => {
-    while (from < 24 && !me[from]) from++;
-  };
-
-  nextPoint();
-
-  while (from < 24 && me[from]) {
-    [comboNo, diceNo] = [0, 0];
-    while (from < 24 && me[from]) {
-      while (from < 24 && comboNo < diceCombo.length) {
-        while (from < 24)
-          while (diceNo < diceCombo[comboNo].length) {
-            const to = from + diceCombo[comboNo][diceNo];
-            if (to < 24 && opp[to] <= 1) {
-              applyMove(from, to);
-            }
-          }
-      }
-      if (diceNo >= diceCombo[comboNo].length) {
-        result.push(branchMoves);
-      }
-      if (to <= 24) {
-        from++;
-      }
-      diceNo = 0;
-      comboNo++;
-    }
-    from++;
-  }
-
-  //diceComboIndex = [0, 0];
-  //return false; // no more moves for this puck
-  return result;
-};
-*/
 const cartesian = (fn, len = []) => {
   const n = len.length;
   let p = new Array(n).fill(0);
@@ -242,89 +65,55 @@ jlog({ l: r.length, r });
 permItems(jlog, true, 2, ["A", "B", "C", "D", "E"]);
 */
 
-/*
-const occPoints = (me, [min = 1, max = 15]) => {
-  for (var i = 0, result = []; i < me.length; i++)
-    if (me[i] >= min && me[i] <= max) result.push(i);
-  return result;
+const doMove = (me, opp) => (from, to) => {
+  me[from]--;
+  me[to]++;
+  opp[0] += opp[to];
+  opp[to] = 0;
 };
-*/
 
-const occPucks = (me, [min = 1, max = 15]) => {
-  for (var i = 0, result = []; i < me.length; i++) {
-    if (me[i] >= min) {
-      for (var c = 0; c < Math.min(me[i], max); c++) result.push(i);
+const undoMove = (me, opp) => (from, to, taken) => {
+  me[from]++;
+  me[to]--;
+  opp[0] -= taken;
+  opp[to] = taken;
+};
+
+const walkMoves = (fn, [d1, d2], me, opp) => {
+  const doM = doMove(me, opp);
+  const undoM = undoMove(me, opp);
+  const combos = d1 === d2 ? [[d1, d1, d1, d1]] : [[d1, d2], [d2, d1]];
+  const end = combos[0].length - 1;
+  const m = me.length;
+  const result = Array(combos[0].length);
+  const recurse = (r, from) => {
+    let to = from + combo[r];
+    while (from < m && to < m) {
+      let taken = opp[to];
+      if (me[from] && taken < 2) {
+        result[r] = [from, to, taken];
+        if (r === end) {
+          fn(result.slice());
+        } else {
+          doM(from, to);
+          recurse(r + 1, from);
+          undoM(from, to, taken);
+        }
+      }
+      from++;
+      to++;
     }
-  }
-  return result;
-};
-
-const occCounts = me => {
-  for (var i = 0, r = []; i < me.length; i++) r.push([i, me[i]]);
-  return r;
-};
-
-const canGo = (opp, to) => to < opp.length && opp[to] < 2;
-
-const puckMoves = (opp, from, dice) => {
-  for (
-    var d = 0, moves = [], taken = [], to = from + dice[d];
-    d < dice.length && to < opp.length && opp[to] < 2;
-
-  ) {
-    if (opp[to]) taken.push(to);
-    moves[d] = { from, to, taken: taken.slice() };
-    d++;
-    to += dice[d];
-  }
-  return moves; //[[singles],[doubles]]
+  };
+  for (var combo of combos) recurse(0, 0);
 };
 
 const validMoves = ({ dice, isWhite, points }) => {
-  let result = [];
-  let [d1, d2] = dice;
-  [d1, d2] = [Math.min(d1, d2), Math.max(d1, d2)];
-  let combos = d1 === d2 ? [[d1, d1, d1, d1]] : [[d1, d2], [d2, d1]];
   let [me, opp] = myPoints({ isWhite, points });
-  const segMoves = [[[], []], [[], []], [[], []], [[], []]]; // singles/doubles
-
-  let starters = [1, combos[0].length].map(i => occPucks(me, [1, i]));
-
-  let p12o4 = [1, combos[0].length].map(limit => occCounts(me, [1, limit]));
-  jlog({ starters, p12o4 });
-  if (d1 !== d2) {
-    combos.forEach((combo, c) =>
-      starters[0].forEach(starter => {
-        const moves = puckMoves(opp, starter, combo);
-        moves.forEach((m1234, i) => segMoves[c][i].push(m1234));
-        jlog2({ c, combo, starter, moves });
-      })
-    );
-  }
-
-  jlog2({ segMoves });
-
-  /*segMoves.map((m, im) =>
-    m.map((g, ig) => jlog({ [`segMoves${im}${ig}`]: g }))
-  );
-
-    //const singles = dicex.map(d => starters[0].filter(e => canGo(opp, e + d)));
-    //const doubles = singles.map(s => s.filter(e => canGo(opp, e + d1 + d2)));
-    //const combos = permArray(dicex.length, starters[1], true);
-    //jlog( dicex, starters, singles, doubles, combos });
-    combos.forEach(d => {
-      if (canGo(opp, d[0] + dicex[0]) && canGo(opp, d[1] + dicex[1])) {
-        result.push(d);
-      }
-    });
-  */
-
-  jlog({ result });
-
-  //jlog(permy(3, 1));
-  //jlog(permArray(dice.length, starters[2]));
-  //jlog(permArray(dice.length, starters[2]).map(p => p.map((e, i) => [e, dice[i]])));
-  return result;
+  const result = [];
+  walkMoves(v => result.push(JSON.stringify(v.sort())), [2, 2], me, opp);
+  result.sort();
+  jlog({ result, len: result.length, slen: [...new Set(result)].length });
+  return [];
 };
 
 export { pips, validMoves };

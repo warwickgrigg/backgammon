9423ce59d86dd3130eaa8a9880ecd9fd46acf953
Warwick Grigg <warwick.grigg@gmail.com>
2020-04-13 10:50:35 +0100

    reduce maybe

--- src/engine/occupation.js
+++ src/engine/occupation.js
@@ -1,3 +1,5 @@
+//const r = pucks => pucks.reduce((a, c) => (c ? a.push(c) : a), []);
+
 export default pucks => {
   for (var i = 0, result = []; i < pucks.length; i++)
     if (pucks[i]) result.push([i, pucks[i]]);
